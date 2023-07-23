09c0e79512564e13f87ff02d5fdde6a286119087
Chirag <chiragnackathaya@gmail.com>
2023-07-23 12:31:17 +0530

    Quiz Website

---
+++ js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Javascript is a  _______ language?",
+    answer: "Object-Based",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "Assembly"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following methods is used to access HTML elements using Javascript?",
+    answer: "Both A and B",
+    options: [
+      "getElementbyId()",
+      "getElementsbyClassName()",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many sizes of headers are available in HTML by default?",
+    answer: "6",
+    options: [
+      "5",
+      "1",
+      "3",
+      "6"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What extension is used to save a HTML program?",
+    answer: ".html",
+    options: [
+      ".ht",
+      ".html",
+      ".hml",
+      ".hl"
+    ]
+  },
+    {
+    numb: 5,
+    question: "A program in HTML can be rendered and read by",
+    answer: "Web browser",
+    options: [
+      "Web browser",
+      "Server",
+      "Interpreter",
+      "Compiler"
+    ]
+  },
+     {
+     numb: 6,
+     question: "The tags in HTML are",
+     answer: "not case sensitive",
+     options: [
+       "case-sensitive",
+       "in upper case",
+      "not case sensitive",
+       "in lowercase"
+     ]
+   },
+   {
+    numb: 7,
+    question: "The CSS property used to change the background color of an element",
+    answer: "background-color",
+    options: [
+      "bgcolor",
+      "color",
+     "background-color",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 8,
+    question: "The CSS property used to specify the transparency of an element",
+    answer: "opacity",
+    options: [
+      "opacity",
+      "filter",
+     "visibility",
+      "overlay"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following is not Javascript datatype?",
+    answer: "All of the above",
+    options: [
+      "Null type",
+      "Undefined type",
+     "Number type",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of the following is the property that is triggered in response to JS errors?",
+    answer: "onerror",
+    options: [
+      "onclick",
+      "onerror",
+     "onmessage",
+      "onexception"
+    ]
+  },
+];
